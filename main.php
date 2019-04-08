0f048f9ef34d3e6f4cdccbab67d78801ccb2ba73
mafruha <44002502+mafruha161@users.noreply.github.com>
2019-04-08 10:27:02 -0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,48 @@
+<?php include('connection.php') ?>
+<!docktype html>
+<html>
+    <head>
+    <title>website  create </title>
+        <link rel="stylesheet" href="style1.css" type="text/css">
+    </head>
+    
+    
+    <body>
+
+        <header>
+       
+            <div>
+            
+            <div class="main">
+                <div class="l">
+                   <h1>Child Career</h1>
+                </div>
+            <ul>
+                 <li><a href="#">join as a sitter</a></li>
+                 <li><a href="login.php">Log in</a></li>
+                 <li><a href="registration.php">sign up</a></li>
+                 <li><a href="#">contact</a></li>
+            
+            </ul>
+            </div>
+
+            <div class="title">
+                <h1>Baby sitter higher</h1>
+            </div>
+
+             <div class="btn">
+
+                 <h2><a href="login.php" class="b">Im A Parent</a></h2>
+                 <h2><a href="registration.php" class="b">Im A Sitter</a></h2>
+             </div>
+
+            </div>  
+            
+        
+        </header>
+    
+    
+    
+    
+    </body>
+</html>
